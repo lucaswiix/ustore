6b8858c7fed863c955899cdc22b5fea0a66c7d4c
lucaswiix <lucasrocha.rosaborges@hotmail.com>
2019-02-27 10:14:12 -0300

    Starter SpringBoot

---
+++ src/main/java/com/ustore/ustore/UstoreApplication.java
@@ -0,0 +1,13 @@
+package com.ustore.ustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ustore/ustore/UstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ustore.ustore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
